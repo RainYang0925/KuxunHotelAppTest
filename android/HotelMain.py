dd6fb3ef6ef6419c0690ca97103993487c85fd91
roisel <rensirui@gmail.com>
2014-07-29 13:46:54 +0800

    added the golob function at the test will setup

--- android/HotelMain.py
+++ android/HotelMain.py
@@ -16,26 +16,29 @@ class HotelMainTest(unittest.TestCase):
         desired_caps['appActivity'] = 'com.kuxun.hotel.HotelMainActivity'
 
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-        time.sleep(2)
+        time.sleep(30)
+
+        self.buttonListMain = getElement.get_buttonList(self.driver)
+        self.textViewListMain = getElement.get_textViewList(self.driver)
 
     def tearDown(self):
         self.driver.quit()
 
     #Test the app startup normal
     def testcase_1_startNormal(self):
-        self.textViewList = getElement.get_textViewList(self.driver)
-        self.assertEqual(self.textViewList[0].text, u'酷讯酒店', 'The app title is wrong')
+        self.assertEqual(self.textViewListMain[0].text, u'酷讯酒店', 'The app title is wrong')
 
     #Test the selectCity button
     def testcase_2_chooseCity(self):
-        self.buttonList = getElement.get_buttonList(self.driver)
-        self.buttonList[4].click()
+        self.buttonListMain[4].click()
         time.sleep(2)
         self.assertEqual(self.driver.current_activity, 'com.kuxun.hotel.HotelSelectCityActivity', 'The activity name is wrong')
 
-    '''
+
     #Test the hotCity click
-    def test_click_hot_city(self):
+    def testcase_3_clickHotCity(self):
+        self.buttonListMain[4].click()
+        time.sleep(2)
         self.buttonListCity = getElement.get_buttonList(self.driver)
         for i in range(len(self.buttonListCity)):
             if u'上海' in self.buttonListCity[i].text:
@@ -45,10 +48,10 @@ class HotelMainTest(unittest.TestCase):
         self.buttonListMain = getElement.get_buttonList(self.driver)
         for i in range(len(self.buttonListMain)):
             print self.buttonListMain[i].text, i
-        print self.buttonListMain[4].text
-        self.assertEqual(self.buttonListMain[4].text, u'上海', 'The city is not right')
+        print self.buttonListMain[6].text
+        self.assertEqual(self.buttonListMain[6].text, u'上海', 'The city is not right')
 
-
+    '''
     #Test the hotel city select
 
     def test_hot_city_select(self):
