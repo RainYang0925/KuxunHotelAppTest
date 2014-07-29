4f4e0fd72bc6157e571de3fa38e2a69e7b190ea5
roisel <rensirui@gmail.com>
2014-07-29 23:59:22 +0800

    added function which will be used in the keyword act

--- android/getElement.py
+++ android/getElement.py
@@ -5,20 +5,23 @@ Home:
 from config import getelements
 
 def get_textViewList(driver):
-    testViewList = driver.find_elements_by_xpath(getelements['textView'])
-    return testViewList
+    return driver.find_elements_by_xpath(getelements['textView'])
 
 def get_textView(driver):
-    textView = driver.find_element_by_xpath(getelements['textView'])
-    return textView
+    return driver.find_element_by_xpath(getelements['textView'])
 
 def get_buttonList(driver):
-    buttonList = driver.find_elements_by_xpath(getelements['buttonView'])
-    return buttonList
+    return driver.find_elements_by_xpath(getelements['buttonView'])
 
 def get_searchCityArea(driver):
-    searchCityResult = driver.find_element_by_xpath(getelements['editText'])
-    return searchCityResult
+    return driver.find_element_by_xpath(getelements['editText'])
+
+def get_RelativeLayoutList(driver):
+    return driver.find_elements_by_xpath(getelements['relativeLayout'])
+
+def get_RelativeTextView(driver, i):
+    return driver.find_element_by_xpath(getelements['relaTextView'] % i)
+
 
 def buttonClick(city, buttonList):
     for i in range(len(buttonList)):
