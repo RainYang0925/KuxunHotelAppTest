f6f8e42bfeb5c8150ca19f3da55844a52ca87c18
roisel <rensirui@gmail.com>
2014-07-29 23:58:26 +0800

    added key to the dictionary

--- android/config.py
+++ android/config.py
@@ -4,5 +4,7 @@
 getelements = {
     'textView':'//android.widget.TextView',
     'buttonView':'//android.widget.Button',
-    'editText':'//android.widget.EditText'
+    'editText':'//android.widget.EditText',
+    'relativeLayout':'//android.widget.RelativeLayout',
+    'relaTextView':'//android.widget.RelativeLayout[%s]/android.widget.TextView'
 }
