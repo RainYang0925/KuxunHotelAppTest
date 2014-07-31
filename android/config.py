55268f8c26492742e74d58ace38c3190304f68ab
roisel <rensirui@gmail.com>
2014-07-31 16:53:10 +0800

    added the dictionary function which will be used in the setup function

--- android/config.py
+++ android/config.py
@@ -1,5 +1,23 @@
 #encoding: utf-8
 
+import devicesOpt
+
+desired_capabilities = {}
+
+def desired_caps():
+
+    if len(list(desired_capabilities)) == 0:
+        desired_capabilities['platformName'] = 'Android'
+        desired_capabilities['platforVersion'] = '4.2'
+
+        #get device info
+        desired_capabilities['deviceName'] = devicesOpt.deviceName()
+
+        #kuxun hotel apk
+        desired_capabilities['appPackage'] = 'com.kuxun.scliang.hotel'
+        desired_capabilities['appActivity'] = 'com.kuxun.hotel.HotelMainActivity'
+
+    return desired_capabilities
 
 getelements = {
     'textView':'//android.widget.TextView',
